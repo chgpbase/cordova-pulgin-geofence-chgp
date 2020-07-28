1e323d3905caf42805cb84e328fd1796c9c5505d
Ilya <chgpbase@gmail.com>
2020-07-28 20:29:08 +0500

    Update GeofencePlugin.java

--- src/android/GeofencePlugin.java
+++ src/android/GeofencePlugin.java
@@ -1,11 +1,8 @@
 package com.cowbell.cordova.geofence;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.os.Bundle;
 import android.util.Log;
 import android.Manifest;
 
@@ -32,7 +29,6 @@ public class GeofencePlugin extends CordovaPlugin {
 
     private GeoNotificationManager geoNotificationManager;
     private Context context;
-    protected static Boolean isInBackground = true;
     public static CordovaWebView webView = null;
 
     private class Action {
@@ -46,21 +42,9 @@ public class GeofencePlugin extends CordovaPlugin {
             this.callbackContext = callbackContext;
         }
     }
-    
+
+    //FIXME: what about many executedActions at once
     private Action executedAction;
-    
-    private GeoNotificationBroadcastReceiver mReceiver = null;
-
-    private class GeoNotificationBroadcastReceiver extends BroadcastReceiver
-    {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Bundle results = getResultExtras(true);
-            results.putBoolean("HANDLED", true);
-            Log.d(TAG, "GeoNotificationBroadcastReceiver - Received Broadcast intent");
-        }
-    }
 
     /**
      * @param cordova
@@ -75,52 +59,6 @@ public class GeofencePlugin extends CordovaPlugin {
         context = this.cordova.getActivity().getApplicationContext();
         Logger.setLogger(new Logger(TAG, context, false));
         geoNotificationManager = new GeoNotificationManager(context);
-    }
-
-    @Override
-    protected void pluginInitialize() {
-        Log.d(TAG, "GeoNotificationBroadcastReceiver - pluginInitialize");
-        super.pluginInitialize();
-        mReceiver = new GeoNotificationBroadcastReceiver();
-        IntentFilter filter = new IntentFilter(ReceiveTransitionsIntentService.GeofenceTransitionIntent);
-        Context context = this.cordova.getActivity();
-        if(context != null)
-        {
-            context.registerReceiver(mReceiver, filter);
-        }
-    }
-
-    @Override
-    public void onResume(boolean multitasking) {
-        Log.d(TAG, "GeoNotificationBroadcastReceiver - onResume");
-        IntentFilter filter = new IntentFilter(ReceiveTransitionsIntentService.GeofenceTransitionIntent);
-        if(context != null && mReceiver != null) {
-            Context context = this.cordova.getActivity();
-            context.registerReceiver(mReceiver, filter);
-        }
-        super.onResume(multitasking);
-    }
-
-    @Override
-    public void onPause(boolean multitasking) {
-        try {
-            Context context = this.cordova.getActivity();
-            context.unregisterReceiver(mReceiver);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        super.onPause(multitasking);
-    }
-
-    @Override
-    public void onDestroy() {
-        Log.d(TAG, "GeoNotificationBroadcastReceiver - onDestroy");
-        try {
-            Context context = this.cordova.getActivity();
-            context.unregisterReceiver(mReceiver);
-        } catch (Exception e) {
-        }
-        super.onDestroy();
     }
 
     @Override
@@ -184,25 +122,13 @@ public class GeofencePlugin extends CordovaPlugin {
 
     private void deviceReady() {
         Intent intent = cordova.getActivity().getIntent();
-        Intent launcherIntent = null;
-        if(intent != null && intent.hasExtra("LAUNCHER_INTENT")){
-            launcherIntent = intent.getParcelableExtra("LAUNCHER_INTENT");
+        String data = intent.getStringExtra("geofence.notification.data");
+        String js = "setTimeout('geofence.onNotificationClicked(" + data + ")',0)";
 
+        if (data == null) {
+            Log.d(TAG, "No notifications clicked.");
         } else {
-            launcherIntent = intent;
-        }
-
-        if(launcherIntent != null){
-
-            String data = launcherIntent.getStringExtra("geofence.notification.data");
-            String js = "setTimeout('geofence.onTransitionReceived(["
-                    + data + "])',0)";
-
-            if (data == null) {
-                Log.d(TAG, "No notifications clicked.");
-            } else {
-                webView.sendJavascript(js);
-            }
+            webView.sendJavascript(js);
         }
     }
 
