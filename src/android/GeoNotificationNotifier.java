373245dac6f0e8cf4264be02b515def59f597877
Ilya <chgpbase@gmail.com>
2020-07-29 14:40:49 +0500

    Update GeoNotificationNotifier.java

--- src/android/GeoNotificationNotifier.java
+++ src/android/GeoNotificationNotifier.java
@@ -4,7 +4,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -29,6 +28,10 @@ public class GeoNotificationNotifier {
             .setVibrate(notification.getVibrate())
             .setSmallIcon(notification.getSmallIcon())
             .setLargeIcon(notification.getLargeIcon())
+            .setStyle(NotificationCompat.BigPictureStyle()
+                    .bigPicture(notification.getImage())
+                    .bigLargeIcon(null))
+            )
             .setAutoCancel(true)
             .setContentTitle(notification.getTitle())
             .setContentText(notification.getText())
