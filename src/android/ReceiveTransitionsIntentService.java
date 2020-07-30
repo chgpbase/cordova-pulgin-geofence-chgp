94f0a902df54272a910ac6556acdee3e5cf32031
Ilya <chgpbase@gmail.com>
2020-07-30 12:28:02 +0500

    Update ReceiveTransitionsIntentService.java

--- src/android/ReceiveTransitionsIntentService.java
+++ src/android/ReceiveTransitionsIntentService.java
@@ -23,7 +23,6 @@ import java.util.List;
 public class ReceiveTransitionsIntentService extends IntentService {
     protected static final String GeofenceTransitionIntent = "com.cowbell.cordova.geofence.TRANSITION";
     public static final String GeofenceActiveQueryIntent = "com.cowbell.cordova.geofence.APP_ACTIVE_QUERY";
-    protected BeepHelper beepHelper;
     protected GeoNotificationNotifier notifier;
     protected GeoNotificationStore store;
 
@@ -32,7 +31,6 @@ public class ReceiveTransitionsIntentService extends IntentService {
      */
     public ReceiveTransitionsIntentService() {
         super("ReceiveTransitionsIntentService");
-        beepHelper = new BeepHelper();
         store = new GeoNotificationStore(this);
         Logger.setLogger(new Logger(GeofencePlugin.TAG, this, false));
     }
